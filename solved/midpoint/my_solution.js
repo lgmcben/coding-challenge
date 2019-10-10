2a8f728b9aeb0e929baaba5405e76ac8fa46d36b
Kitti Wateesatogkij <lgmcben@gmail.com>
2019-10-10 17:43:56 +0700

    feat: Add solution for finding mid-point of a linked list
    also change folder structure

---
+++ solved/midpoint/my_solution.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Return the 'middle' node of a linked list.
+// If the list has an even number of elements, return
+// the node at the end of the first half of the list.
+// *Do not* use a counter variable, *do not* retrieve
+// the size of the list, and only iterate
+// through the list one time.
+// --- Example
+//   const l = new LinkedList();
+//   l.insertLast('a')
+//   l.insertLast('b')
+//   l.insertLast('c')
+//   midpoint(l); // returns { data: 'b' }
+
+function midpoint(list) {
+    if(list.size()===0){
+        return null;
+    }
+    let slowPointer = list.getFirst();
+    let fastPointer = list.getFirst();
+    while(fastPointer.next && fastPointer.next.next) {
+        slowPointer = slowPointer.next;
+        fastPointer = fastPointer.next.next;
+    }
+    return slowPointer;
+}
+
+module.exports = midpoint;
