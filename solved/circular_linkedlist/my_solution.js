90927ebb864ca45b40dac193c3daa5334edb4f3c
Kitti Wateesatogkij <lgmcben@gmail.com>
2019-10-11 14:39:42 +0700

    feat: Add solution to circular linkedlist problem

---
+++ solved/circular_linkedlist/my_solution.js
@@ -0,0 +1,28 @@
+// --- Directions
+// Given a linked list, return true if the list
+// is circular, false if it is not.
+// --- Examples
+//   const l = new List();
+//   const a = new Node('a');
+//   const b = new Node('b');
+//   const c = new Node('c');
+//   l.head = a;
+//   a.next = b;
+//   b.next = c;
+//   c.next = b;
+//   circular(l) // true
+
+function circular(list) {
+    let slowPointer = list.getFirst();
+    let fastPointer = list.getFirst();
+    while(fastPointer.next && fastPointer.next.next) {
+        slowPointer = slowPointer.next;
+        fastPointer = fastPointer.next.next;
+        if(slowPointer === fastPointer){
+            return true
+        }
+    }
+    return false;
+}
+
+module.exports = circular;
