74a29a6396595d1cf82a651d3885a91ea8e9bfc8
Kitti Wateesatogkij <lgmcben@gmail.com>
2019-10-22 19:53:08 +0700

    feat: Add solution for breadth first traversal

--- solved/tree/my_solution.js
+++ solved/tree/my_solution.js
@@ -30,6 +30,40 @@ class Node {
     }
 }
 
-class Tree {}
+class Tree {
+    constructor() {
+        this.root = null;
+    }
+    
+    traverseBF(fn) {
+        if(!this.root) {
+            return;
+        }
+
+        let holder = [this.root];
+
+        while(holder.length > 0) {
+            const node = holder.shift();
+            holder = [...holder, ...node.children];
+            fn(node);
+        }
+    }
+}
+
+
+const node1 = new Node(10);
+node1.add(10);
+node1.add(20);
+node1.add(30);
+node1.children[0].add(40);
+node1.children[0].add(50);
+node1.children[0].add(60);
+node1.children[1].add(70);
+node1.children[1].add(80);
+node1.children[1].add(90);
+
+const tree = new Tree();
+tree.root = node1;
+tree.traverseBF((node)=>console.log(`current node data = ${node.data}`) );
 
 module.exports = { Tree, Node };
