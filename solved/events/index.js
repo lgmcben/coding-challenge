5e45b027f2e857d9acc75f20c55f6883c714b02b
Kitti Wateesatogkij <lgmcben@gmail.com>
2019-10-31 11:46:46 +0700

    feat: Add solution for basic eventing

---
+++ solved/events/index.js
@@ -0,0 +1,33 @@
+// --- Directions
+// Create an 'eventing' library out of the
+// Events class.  The Events class should
+// have methods 'on', 'trigger', and 'off'.
+
+class Events {
+  constructor() {
+    this.events = {};
+  }
+  // Register an event handler
+  on(eventName, callback) {
+    if(!this.events[eventName]) {
+      this.events[eventName] = [];
+    }
+    this.events[eventName].push(callback);
+  }
+
+  // Trigger all callbacks associated
+  // with a given eventName
+  trigger(eventName) {
+    for(let fn of this.events[eventName]){
+      fn();
+    }
+  }
+
+  // Remove all event handlers associated
+  // with the given eventName
+  off(eventName) {
+    this.events[eventName] = [];
+  }
+}
+
+module.exports = Events;
